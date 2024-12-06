9ec4fbf595826dd9cd671ebfe9c8779e6cbbf4cb
Venkkatesh Sekar <venkkatesh.sekar@dfinity.org>
2024-12-06 12:46:05 +0000

    init

---
+++ src/static_backend/src/asset.rs
@@ -0,0 +1,111 @@
+use ic_asset_certification::{Asset, AssetConfig, AssetEncoding, AssetFallbackConfig, AssetRouter};
+use ic_cdk::api::{data_certificate, set_certified_data};
+use ic_http_certification::{
+    HeaderField, HttpCertificationTree, HttpRequest, HttpResponse, StatusCode,
+};
+use include_dir::{include_dir, Dir};
+use std::borrow::Cow;
+use std::{cell::RefCell, rc::Rc};
+
+use crate::{serve_canister_info, ENABLE_TEMPLATING};
+
+thread_local! {
+    static HTTP_TREE: Rc<RefCell<HttpCertificationTree>> = Default::default();
+    static ASSET_ROUTER: RefCell<AssetRouter<'static>> = RefCell::new(AssetRouter::with_tree(HTTP_TREE.with(|tree| tree.clone())));
+}
+
+static ASSETS_DIR: Dir<'_> = include_dir!("src/assets");
+const IMMUTABLE_ASSET_CACHE_CONTROL: &str = "public, max-age=31536000, immutable";
+
+pub(crate) fn serve_asset(req: &HttpRequest) -> HttpResponse<'static> {
+    ASSET_ROUTER.with_borrow(|asset_router| {
+        if let Ok(response) = asset_router.serve_asset(
+            &data_certificate().expect("No data certificate available"),
+            req,
+        ) {
+            response
+        } else {
+            ic_cdk::trap("Failed to serve asset");
+        }
+    })
+}
+
+pub(crate) async fn certify_all_assets() {
+    let encodings = vec![
+        AssetEncoding::Brotli.default_config(),
+        AssetEncoding::Gzip.default_config(),
+    ];
+
+    let asset_configs = vec![
+        AssetConfig::File {
+            path: "index.html".to_string(),
+            content_type: Some("text/html".to_string()),
+            headers: get_asset_headers(vec![(
+                "cache-control".to_string(),
+                "public, no-cache, no-store".to_string(),
+            )]),
+            fallback_for: vec![AssetFallbackConfig {
+                scope: "/".to_string(),
+                status_code: Some(StatusCode::OK),
+            }],
+            aliased_by: vec!["/".to_string()],
+            encodings: encodings.clone(),
+        },
+        AssetConfig::Pattern {
+            pattern: "**/*.js".to_string(),
+            content_type: Some("text/javascript".to_string()),
+            headers: get_asset_headers(vec![(
+                "cache-control".to_string(),
+                IMMUTABLE_ASSET_CACHE_CONTROL.to_string(),
+            )]),
+            encodings: encodings.clone(),
+        },
+        AssetConfig::Pattern {
+            pattern: "**/*.css".to_string(),
+            content_type: Some("text/css".to_string()),
+            headers: get_asset_headers(vec![(
+                "cache-control".to_string(),
+                IMMUTABLE_ASSET_CACHE_CONTROL.to_string(),
+            )]),
+            encodings,
+        },
+    ];
+
+    let mut assets = Vec::new();
+    for file in ASSETS_DIR.files() {
+        let path = file.path().to_string_lossy();
+        // Special case for templating
+        if path.ends_with("index.hbs") {
+            if ENABLE_TEMPLATING {
+                let asset = Cow::Owned(serve_canister_info(file).await.as_bytes().to_vec());
+                assets.push(Asset::new("index.html", asset));
+            } else {
+                assets.push(Asset::new("index.html", file.contents()));
+            }
+        } else {
+            assets.push(Asset::new(path, file.contents()));
+        }
+    }
+
+    ASSET_ROUTER.with_borrow_mut(|asset_router| {
+        if let Err(err) = asset_router.certify_assets(assets, asset_configs) {
+            ic_cdk::trap(&format!("Failed to certify assets: {}", err));
+        }
+        set_certified_data(&asset_router.root_hash());
+    });
+}
+
+fn get_asset_headers(additional_headers: Vec<HeaderField>) -> Vec<HeaderField> {
+    let mut headers = vec![
+        ("strict-transport-security".to_string(), "max-age=31536000; includeSubDomains".to_string()),
+        ("x-frame-options".to_string(), "DENY".to_string()),
+        ("x-content-type-options".to_string(), "nosniff".to_string()),
+        ("content-security-policy".to_string(), "default-src 'self'; img-src 'self' data:; form-action 'self'; object-src 'none'; frame-ancestors 'none'; upgrade-insecure-requests; block-all-mixed-content".to_string()),
+        ("referrer-policy".to_string(), "no-referrer".to_string()),
+        ("permissions-policy".to_string(), "accelerometer=(),ambient-light-sensor=(),autoplay=(),battery=(),camera=(),display-capture=(),document-domain=(),encrypted-media=(),fullscreen=(),gamepad=(),geolocation=(),gyroscope=(),layout-animations=(self),legacy-image-formats=(self),magnetometer=(),microphone=(),midi=(),oversized-images=(self),payment=(),picture-in-picture=(),publickey-credentials-get=(),speaker-selection=(),sync-xhr=(self),unoptimized-images=(self),unsized-media=(self),usb=(),screen-wake-lock=(),web-share=(),xr-spatial-tracking=()".to_string()),
+        ("cross-origin-embedder-policy".to_string(), "require-corp".to_string()),
+        ("cross-origin-opener-policy".to_string(), "same-origin".to_string()),
+    ];
+    headers.extend(additional_headers);
+    headers
+}
--- src/static_backend/src/lib.rs
+++ src/static_backend/src/lib.rs
@@ -1,4 +1,126 @@
+use candid::{Nat, Principal};
+use chrono::prelude::*;
+use handlebars::Handlebars;
+use ic_cdk::api::management_canister::main::{canister_status, CanisterStatusResponse};
+use ic_cdk::api::management_canister::main::{CanisterIdRecord, CanisterStatusType, LogVisibility};
+use ic_http_certification::{HttpRequest, HttpResponse};
+use include_dir::File;
+use num_traits::cast::ToPrimitive;
+use serde::{Deserialize, Serialize};
+use std::time::Duration;
+
+mod asset;
+
+pub const ENABLE_TEMPLATING: bool = true;
+const UPDATE_INTERVAL_SECS: u64 = 120;
+
+#[ic_cdk::init]
+fn init() {
+    certify_on_timer();
+}
+
+#[ic_cdk::post_upgrade]
+fn post_upgrade() {
+    certify_on_timer();
+}
+
+fn certify_on_timer() {
+    ic_cdk_timers::set_timer_interval(Duration::from_secs(UPDATE_INTERVAL_SECS), || {
+        ic_cdk::spawn(async {
+            let now = ic_cdk::api::performance_counter(1);
+            asset::certify_all_assets().await;
+            let elapsed = ic_cdk::api::performance_counter(1);
+            ic_cdk::println!("Instructions used for recertification : {}", elapsed - now);
+        })
+    });
+}
+
 #[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+fn http_request(req: HttpRequest) -> HttpResponse {
+    let _path = req.get_path().expect("Failed to parse request path");
+    asset::serve_asset(&req)
+}
+
+async fn serve_canister_info<'a>(file: &File<'a>) -> String {
+    let response = canister_status(CanisterIdRecord {
+        canister_id: ic_cdk::id(),
+    })
+    .await
+    .unwrap()
+    .0;
+
+    let mut reg = Handlebars::new();
+    let source = file.contents();
+    assert!(reg
+        .register_template_string("metrics", String::from_utf8_lossy(source))
+        .is_ok());
+
+    let mut definite_response = DefiniteCanisterStatus::from(response);
+    definite_response.last_updated_at = timestamp(ic_cdk::api::time());
+    reg.render("metrics", &definite_response).unwrap()
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct DefiniteCanisterStatus {
+    pub status: CanisterStatusType,
+    pub module_hash: String,
+    pub memory_size: u64,
+    pub cycles: u64,
+    pub idle_cycles_burned_per_day: u64,
+    pub reserved_cycles: u64,
+
+    pub query_num_calls_total: u64,
+    pub query_num_instructions_total: u64,
+    pub query_request_payload_bytes_total: u64,
+    pub query_response_payload_bytes_total: u64,
+
+    pub controllers: Vec<Principal>,
+    pub compute_allocation: u64,
+    pub memory_allocation: u64,
+    pub freezing_threshold: u64,
+    pub reserved_cycles_limit: u64,
+    pub log_visibility: LogVisibility,
+    pub wasm_memory_limit: u64,
+
+    pub last_updated_at: String,
+}
+
+impl From<CanisterStatusResponse> for DefiniteCanisterStatus {
+    fn from(value: CanisterStatusResponse) -> Self {
+        Self {
+            status: value.status,
+            module_hash: hex::encode(value.module_hash.expect("Wasm should exist")),
+            memory_size: nu64(value.memory_size),
+            cycles: nu64(value.cycles),
+            idle_cycles_burned_per_day: nu64(value.idle_cycles_burned_per_day),
+            reserved_cycles: nu64(value.reserved_cycles),
+
+            query_num_calls_total: nu64(value.query_stats.num_calls_total),
+            query_num_instructions_total: nu64(value.query_stats.num_instructions_total),
+            query_request_payload_bytes_total: nu64(value.query_stats.request_payload_bytes_total),
+            query_response_payload_bytes_total: nu64(
+                value.query_stats.response_payload_bytes_total,
+            ),
+
+            controllers: value.settings.controllers,
+            compute_allocation: nu64(value.settings.compute_allocation),
+            memory_allocation: nu64(value.settings.memory_allocation),
+            freezing_threshold: nu64(value.settings.freezing_threshold),
+            reserved_cycles_limit: nu64(value.settings.reserved_cycles_limit),
+            log_visibility: value.settings.log_visibility,
+            wasm_memory_limit: nu64(value.settings.wasm_memory_limit),
+
+            last_updated_at: String::new(),
+        }
+    }
+}
+
+fn nu64(num: Nat) -> u64 {
+    num.0.to_u64().expect("Nat doesn't fit into u64")
+}
+
+fn timestamp(time: u64) -> String {
+    let timestamp = time as i64;
+    let datetime = DateTime::from_timestamp_nanos(timestamp);
+    datetime.format("%Y-%m-%d %H:%M:%S").to_string()
 }
